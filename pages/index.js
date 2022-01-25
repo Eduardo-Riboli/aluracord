b8d54abefec01107fdecd6e872859b46d757ce40
Eduardo <EduardoRiboli71@gmail.com>
2022-01-25 19:14:20 -0300

    segunda parte

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,10 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
 
 function Titulo(props) {
-    console.log(props);
     const Tag = props.tag || 'h1';
     return (
         <>
@@ -64,11 +37,13 @@ function HomePage() {
   export default HomePage */
 
 export default function PaginaInicial() {
-    const username = 'Carkis';
+    //const username = 'Joao';
+    const [username, setUsername] = React.useState('Eduardo-Riboli')
+    const roteamento = useRouter();
+
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -95,6 +70,12 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (evento) {
+                            evento.preventDefault();
+                            console.log('Alguem submeteu');
+                            roteamento.push('/chat')
+                            //window.location.href = '/chat'
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -105,7 +86,28 @@ export default function PaginaInicial() {
                             {appConfig.name}
                         </Text>
 
+                        {/* <input 
+                        type="text" 
+                        value={username} 
+                        onChange={function (evento) {
+                            console.log('usuario digitou', evento.target.value);
+                            // Onde ta o valor?
+                            const valor = evento.target.value;
+                            // Trocar o valor da variável
+                            // através do react
+                            setUsername(valor);
+                        }} 
+                        />*/}
                         <TextField
+                            value={username} 
+                            onChange={function (evento) {
+                                console.log('usuario digitou', evento.target.value);
+                                // Onde ta o valor?
+                                const valor = evento.target.value;
+                                // Trocar o valor da variável
+                                // através do react
+                                setUsername(valor);
+                            }} 
                             fullWidth
                             textFieldColors={{
                                 neutral: {
@@ -115,8 +117,9 @@ export default function PaginaInicial() {
                                     backgroundColor: appConfig.theme.colors.neutrals[800],
                                 },
                             }}
-                        />
+                        /> 
                         <Button
+                           // href='/chat'
                             type='submit'
                             label='Entrar'
                             fullWidth
